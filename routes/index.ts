65e603c497b386e469aa9fda8304ef26ef8ad5d3
victor rivera <soma0112@hotmail.com>
2022-07-28 05:13:58 +0000

    Upload New File

---
+++ routes/index.ts
@@ -0,0 +1,25 @@
+/* import { Router } from 'express'
+import saludosRoutes from './authetication/index'
+
+const router = Router()
+
+router.use('/api/saludo', saludosRoutes)
+router.use('/api/auth', AuthRouter) 
+
+export default router*/
+
+import { Router } from "express";
+
+import authorization from '../helpers/jwt';
+
+import { AuthRouter } from "../auth/auth.routes";
+import { SaludosRouter } from "../saludos/saludos.routes";
+import { adminUsersRouter } from "../users/users.routes";
+
+export const mainRouter = Router();
+
+mainRouter
+    .use('/auth', AuthRouter)
+    .use('/saludo', SaludosRouter)
+    //.use('/', authorization.verifyToken)
+    .use('/users', adminUsersRouter);
