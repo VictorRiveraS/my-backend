064dd6aee8f93109d67d3581ad983ec5d9d11c79
victor rivera <soma0112@hotmail.com>
2022-07-28 05:22:02 +0000

    Delete users.routes.ts

--- saludos/users.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import ctrl from './users.ctrl';
-import RouteValidator from '../helpers/validators';
-import { changePasswordValidator } from './users.validator';
-
-export const adminUsersRouter = Router();
-
-adminUsersRouter
-    .patch('/change-password', changePasswordValidator, RouteValidator.validate, ctrl.changePassword);
