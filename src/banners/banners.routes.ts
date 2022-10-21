dc76abab3d86eeb8d9608b872ccc2e46a4b407ef
VictorRivera-BM <victor.rivera@bambu-mobile.com>
2022-10-21 02:51:54 -0500

    changes

--- src/banners/banners.routes.ts
+++ src/banners/banners.routes.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import ctrl from './banners.ctrl';
 import RouteValidator from '../helpers/validators';
 import { addBannerImageValidator, bannerssValidator } from './banners.validator';
-import { uploadImageS3 } from '../aws-s3-bucket/upload-s3.service';
+//import { uploadImageS3 } from '../aws-s3-bucket/upload-s3.service';
 
 export const BannersRoutes = Router();
 
@@ -10,6 +10,6 @@ BannersRoutes
     .get('/', RouteValidator.validate, ctrl.fetchBanners)
     .get('/id', bannerssValidator, RouteValidator.validate, ctrl.getBannerById)
     .post('/', RouteValidator.validate, ctrl.createBanner)
-    .post('/picture-banner', ctrl.setInfoUpload, uploadImageS3.single('file'), addBannerImageValidator, RouteValidator.validate, ctrl.addNewsImage)
+    //.post('/picture-banner', ctrl.setInfoUpload, uploadImageS3.single('file'), addBannerImageValidator, RouteValidator.validate, ctrl.addNewsImage)
     .patch('/', bannerssValidator, RouteValidator.validate, ctrl.updateBanner)
     .delete('/', bannerssValidator, RouteValidator.validate, ctrl.deleteBanner);
